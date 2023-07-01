9943dc46f71a3e0da7da7f4be0b4eddcec41f774
Guilherme Bley <guilhermecbley@gmail.com>
2023-06-30 23:08:06 -0300

    result builder

---
+++ src/Services/Catalog/Ecommerce.Catalog.Core/Primitive/Result/ResultBuilder.cs
@@ -0,0 +1,92 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace ECommerce.Catalog.Core.Primitive.Result;
+
+public sealed class ResultBuilder<TResult>
+{
+    public const int DEFAULT_CODE = 400;
+
+    private readonly List<ICoreError> _errors = new();
+    public bool HasError => _errors.Any();
+
+    public ResultBuilder()
+    {
+    }
+
+    public ResultBuilder(ResultBuilder<TResult> resultBuilder)
+    {
+        _errors.AddRange(resultBuilder._errors);
+    }
+
+    public ResultBuilder(IEnumerable<ICoreError> errors)
+    {
+        _errors.AddRange(errors);
+    }
+
+    public void AddIfNullOrWhiteSpace(string? str, string message, int code = DEFAULT_CODE)
+        => AddIf(string.IsNullOrWhiteSpace(str), message, code);
+
+    public void AddIfNullOrEmpty(string? str, string message, int code = DEFAULT_CODE)
+        => AddIf(string.IsNullOrEmpty(str), message, code);
+
+    public void AddIfNull(object? obj, string message, int code = DEFAULT_CODE)
+        => AddIf(obj is null, message, code);
+
+    public void AddIf(bool condition, string message, int code = DEFAULT_CODE)
+    {
+        if (condition)
+            Add(message, code);
+    }
+
+    public void Add(string message, int code = DEFAULT_CODE)
+        => _errors.Add(new Error(code, message));
+
+    public void AddRange(params (string Message, int Code)[] values)
+        => _errors.AddRange(values.Select(value => new Error(value.Code, value.Message)));
+
+    public Result<TResult> Failed()
+    {
+        if (!HasError)
+            throw new InvalidOperationException("There aren't errors.");
+
+        return Result<TResult>.Failed(_errors.AsReadOnly());
+    }
+
+    public Result<TResult> Success(TResult result)
+    {
+        if (result is null)
+            throw new ArgumentNullException("result");
+
+        if (HasError)
+            throw new InvalidOperationException("There aren't errors.");
+
+        return Result<TResult>.Success(result);
+    }
+
+    public bool TrySuccess(TResult result, [NotNullWhen(true)] out Result<TResult>? resultValue)
+    {
+        resultValue = null;
+
+        if (HasError)
+            return false;
+
+        if (result is null)
+            return false;
+
+        resultValue = Result<TResult>.Success(result); 
+        return true;
+    }
+
+    public bool TryFailed([NotNullWhen(true)] out Result<TResult>? resultValue)
+    {
+        resultValue = null;
+
+        if (!HasError)
+            return false;
+
+        resultValue = Result<TResult>.Failed(_errors.AsReadOnly()); 
+        return true;
+    }
+
+    private record Error(int Code, string Message) : ICoreError;
+}
