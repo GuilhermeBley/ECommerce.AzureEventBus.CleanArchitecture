520c9c171026916f1421f9415be7ff671b785b8d
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-08 22:42:56 -0300

    remove notnull values

--- src/Services/Catalog/Ecommerce.Catalog.Application/Model/Identity/RoleModel.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Model/Identity/RoleModel.cs
@@ -3,6 +3,6 @@
 public class RoleModel
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string NormalizedName { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string NormalizedName { get; set; } = string.Empty;
 }
