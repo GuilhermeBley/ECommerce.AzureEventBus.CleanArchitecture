e4f4b7c69ca0e18038b327a043f34169095c68c1
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-21 23:54:13 -0300

    roleuserclient

---
+++ src/Services/Catalog/Ecommerce.Catalog.Core/Entities/Identity/RoleUserClaim.cs
@@ -0,0 +1,36 @@
+﻿namespace Ecommerce.Catalog.Core.Entities.Identity;
+
+public class RoleUserClaim
+{
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid RoleId { get; private set; }
+
+    private RoleUserClaim(Guid id, Guid userId, Guid roleId)
+    {
+        Id = id;
+        UserId = userId;
+        RoleId = roleId;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is RoleUserClaim claim &&
+               Id.Equals(claim.Id) &&
+               UserId.Equals(claim.UserId) &&
+               RoleId.Equals(claim.RoleId);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, UserId, RoleId);
+    }
+
+    public static Result<RoleUserClaim> Create(Guid id, User user, Role role)
+        => Create(id, user.Id, role.Id);
+
+    public static Result<RoleUserClaim> Create(Guid id, Guid userId, Guid roleId)
+        => Result<RoleUserClaim>.Success(
+            new RoleUserClaim(id, userId, roleId)
+        );
+}
