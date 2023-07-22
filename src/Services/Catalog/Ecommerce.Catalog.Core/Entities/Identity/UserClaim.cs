0b7fbeb891cbbf2360ab607ea2307564759db2af
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-22 00:19:01 -0300

    company claims

--- src/Services/Catalog/Ecommerce.Catalog.Core/Entities/Identity/UserClaim.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Core/Entities/Identity/UserClaim.cs
@@ -2,8 +2,8 @@
 
 public class UserClaim : ClaimEntity
 {
-    public Guid Id { get; set; }
-    public Guid UserId { get; set; }
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
 
     private UserClaim(Guid id, Guid userId, string claimType, string claimValue)
     {
