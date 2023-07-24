af82591e720277387001bf9e339392fd798de879
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-23 23:44:19 -0300

    Create models #9

---
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Model/Company/CompanyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Catalog.Application.Model.Company;
+
+public class CompanyModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime? UpdateAt { get; set; }
+    public DateTime CreateAt { get; set; }
+}
---
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Model/Company/CompanyUserClaimModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Catalog.Application.Model.Company;
+
+public class CompanyUserClaimModel
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid CompanyId { get; set; }
+    public string ClaimType { get; set; } = string.Empty;
+    public string ClaimValue { get; set; } = string.Empty;
+}
