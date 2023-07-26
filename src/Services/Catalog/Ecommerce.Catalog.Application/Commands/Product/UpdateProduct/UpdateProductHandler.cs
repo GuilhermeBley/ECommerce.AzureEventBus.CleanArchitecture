b6b29a502ce766a20501209b19611b4c0183352f
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-25 23:51:45 -0300

    Handle update product #4

---
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Commands/Product/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,77 @@
+﻿using Ecommerce.Catalog.Application.Commands.Product.CreateProduct;
+using Ecommerce.Catalog.Application.Commands.Product.DeleteProduct;
+using Ecommerce.Catalog.Application.Repositories;
+using Ecommerce.Catalog.Core.Entities.Product;
+using Ecommerce.Catalog.Core.Extension;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Ecommerce.Catalog.Application.Commands.Product.UpdateProduct;
+
+public class UpdateProductHandler : IAppRequestHandler<UpdateProductRequest, Result<UpdateProductResponse>>
+{
+    private readonly CatalogContext _catalogContext;
+    private readonly ClaimsPrincipal _principal;
+
+    public UpdateProductHandler(ClaimsPrincipal principal, CatalogContext catalogContext)
+    {
+        _catalogContext = catalogContext;
+        _principal = principal;
+    }
+
+    public async Task<Result<UpdateProductResponse>> Handle(UpdateProductRequest request, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var claimCompany = _principal.GetCompany();
+
+        if (!claimCompany.TryGetValue(out Guid companyId))
+            return Result<UpdateProductResponse>.Failed(claimCompany.Errors);
+
+        using var transaction = await _catalogContext.Database.BeginTransactionAsync();
+
+        var productToUpdate = await
+            (from companyPro in _catalogContext.CompanyProducts
+            join prod in _catalogContext.Products
+                on companyPro.ProductId equals prod.Id
+            where companyPro.CompanyId == companyId
+            select prod)
+            .FirstOrDefaultAsync(c => c.Id == request.ProductId);
+
+        if (productToUpdate is null)
+            return ResultBuilderExtension
+                .CreateFailed<UpdateProductResponse>(Core.Exceptions.ErrorEnum.ProductNotFound);
+
+        var resultEntityProduct = Core.Entities.Product.Product.Create(
+            id: productToUpdate.Id,
+            name: request.NewName,
+            description: request.NewDescription,
+            value: request.NewValue,
+            updateAt: DateTime.UtcNow,
+            insertAt: productToUpdate.InsertAt
+        );
+
+        if (resultEntityProduct.IsFailure ||
+            resultEntityProduct.Value is null)
+            return Result<UpdateProductResponse>.Failed(resultEntityProduct.Errors);
+
+        productToUpdate.Value = resultEntityProduct.Value.Value;
+        productToUpdate.UpdateAt = resultEntityProduct.Value.UpdateAt;
+        productToUpdate.Description = resultEntityProduct.Value.Description;
+        productToUpdate.Name = resultEntityProduct.Value.Name;
+
+        await transaction.CommitAsync();
+        await _catalogContext.SaveChangesAsync();
+
+        return Result<UpdateProductResponse>.Success(
+                new UpdateProductResponse
+                {
+                    Description = productToUpdate.Description,
+                    Name = productToUpdate.Name,
+                    InsertAt = productToUpdate.InsertAt,
+                    UpdateAt = productToUpdate.UpdateAt,
+                    Value = productToUpdate.Value
+                }
+            );
+    }
+}
---
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Commands/Product/UpdateProduct/UpdateProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Catalog.Application.Commands.Product.UpdateProduct;
+
+public class UpdateProductRequest
+{
+    public Guid ProductId { get; set; }
+    public string NewName { get; set; } = string.Empty;
+    public string NewDescription { get; set; } = string.Empty;
+    public double NewValue { get; set; }
+}
