31a11673b09085b80e582905e4e1870ac431e598
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-28 23:50:55 -0300

    Create event bus abstractions #13

--- src/Services/Catalog/Ecommerce.Catalog.Application/Mediator/IAppRequestHandler.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Mediator/IAppRequestHandler.cs
@@ -1,7 +1,5 @@
 ﻿namespace Ecommerce.Catalog.Application.Mediator;
 
-public interface 
-
 public interface IAppRequestHandler<in TRequest, TResponse>
 {
     Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
--- src/Services/Catalog/Ecommerce.Catalog.Application/globalusings.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/globalusings.cs
@@ -1,4 +1,6 @@
 ﻿global using Ecommerce.Catalog.Application.Mediator;
 global using Ecommerce.Catalog.Core.Primitive;
+global using Ecommerce.EventBus.Abstractions;
+global using Ecommerce.EventBus;
 global using Ecommerce.Catalog.Core.Primitive.Result;
 global using Ecommerce.Catalog.Application.Security.Extensions;
