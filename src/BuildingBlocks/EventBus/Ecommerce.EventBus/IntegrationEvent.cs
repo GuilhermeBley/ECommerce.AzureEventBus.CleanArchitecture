1802b24fde4cc2e781eae6b7d043c55faad9ac2e
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-28 23:48:14 -0300

    Create event bus abstractions #13

---
+++ src/BuildingBlocks/EventBus/Ecommerce.EventBus/IntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Ecommerce.EventBus;
+
+public class IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        EventId = Guid.NewGuid();
+        EventCreationDate = DateTime.UtcNow;
+    }
+
+    public IntegrationEvent(Guid id, DateTime createDate)
+    {
+        EventId = id;
+        EventCreationDate = createDate;
+    }
+
+    public Guid EventId { get; private set; }
+
+    public DateTime EventCreationDate { get; private set; }
+}
