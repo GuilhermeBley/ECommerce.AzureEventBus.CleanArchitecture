78a9ee26a75f50da9b1e5802a1802c7c50305636
Guilherme Bley <guilhermecbley@gmail.com>
2023-08-25 22:25:28 -0300

    event bus extension di

---
+++ src/BuildingBlocks/EventBus/Ecommerce.EventBus.Azure/Extensions/Di/EventBusServiceBusExtension.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.EventBus.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Ecommerce.EventBus.Azure.Extensions.Di;
+
+public static class EventBusServiceBusExtension
+{
+    public static IServiceCollection AddEventBus(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IEventBus>(
+            provider => {
+
+                var eventBusOptions = provider.GetRequiredService<IOptions<AzureServiceBusOptions>>();
+                var logFactory = provider.GetRequiredService<ILoggerFactory>();
+
+                var serviceBusPersisterConnection
+                    = new DefaultServiceBusPersisterConnection(
+                        logger: logFactory.CreateLogger<DefaultServiceBusPersisterConnection>(),
+                        options: eventBusOptions
+                    );
+
+                var subsManager = new InMemoryEventBusSubscriptionsManager();
+
+                return new EventBusServiceBus(
+                    serviceBusPersisterConnection: serviceBusPersisterConnection,
+                    logger: logFactory.CreateLogger<EventBusServiceBus>(),
+                    subsManager: subsManager,
+                    options: eventBusOptions,
+                    provider: provider.CreateScope().ServiceProvider);
+            });
+
+        return serviceCollection;
+    }
+}
