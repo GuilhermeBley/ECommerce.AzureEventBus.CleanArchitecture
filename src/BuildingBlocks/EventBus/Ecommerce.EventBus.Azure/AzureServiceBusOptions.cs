2514049aad4aa96e66f47ce6c362b44721db6119
Guilherme Bley <guilhermecbley@gmail.com>
2023-08-25 22:50:50 -0300

    add event bus to catalog api

--- src/BuildingBlocks/EventBus/Ecommerce.EventBus.Azure/AzureServiceBusOptions.cs
+++ src/BuildingBlocks/EventBus/Ecommerce.EventBus.Azure/AzureServiceBusOptions.cs
@@ -2,6 +2,8 @@
 
 public class AzureServiceBusOptions
 {
+    public const string SECTION = "AzureServiceBus";
+
     public string ConnectionString { get; set; } = string.Empty;
     public string Subscription { get; set; } = string.Empty;
     public string TopicName { get; set; } = string.Empty;
--- src/Services/Catalog/Ecommerce.Catalog.Api/Program.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Api/Program.cs
@@ -1,3 +1,5 @@
+using Ecommerce.EventBus.Azure.Extensions.Di;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEventBus();
+
+builder.Services.Configure<Ecommerce.EventBus.Azure.AzureServiceBusOptions>(
+     builder.Configuration.GetSection(Ecommerce.EventBus.Azure.AzureServiceBusOptions.SECTION));
 
 var app = builder.Build();
 
