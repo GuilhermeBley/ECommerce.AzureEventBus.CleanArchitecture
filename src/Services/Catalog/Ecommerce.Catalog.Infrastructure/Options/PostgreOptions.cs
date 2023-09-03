df433ad1fa5ce7103722a562d29a451d7e9beb0d
Guilherme Bley <guilhermecbley@gmail.com>
2023-09-02 22:26:50 -0300

    add secrets to api

--- src/Services/Catalog/Ecommerce.Catalog.Infrastructure/Options/PostgreOptions.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Infrastructure/Options/PostgreOptions.cs
@@ -2,7 +2,7 @@
 
 public class PostgreOptions
 {
-    public const string SECTION = "Postgre";
+    public const string SECTION = "Postgres";
 
     public string ConnectionString { get; set; } = string.Empty;
 }
