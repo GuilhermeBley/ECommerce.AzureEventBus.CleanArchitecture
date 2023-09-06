764f0b5762d0fe902884acd3e54be4e2808b2650
Guilherme Bley <guilhermecbley@gmail.com>
2023-09-06 16:57:51 -0300

    add debug properties

--- src/Services/Catalog/Ecommerce.Catalog.Infrastructure/Mediator/MediatRAdapter.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Infrastructure/Mediator/MediatRAdapter.cs
@@ -1,8 +1,10 @@
 ﻿using Ecommerce.Catalog.Application.Mediator;
 using MediatR;
+using System.Diagnostics;
 
 namespace Ecommerce.Catalog.Infrastructure.Mediator;
 
+[DebuggerNonUserCode]
 internal class MediatRAdapter : IAppMediator
 {
     private readonly IMediator myImpl;
