8c5bb5bb8d04907e031ee4b53742fc947b5345c1
Guilherme Bley <guilhermecbley@gmail.com>
2023-09-22 23:05:07 -0300

    Create repositories - repository abstraction #29

---
+++ src/Services/Identity/Ecommerce.Identity.Application/Repositories/IdentityContext.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Identity.Application.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Identity.Application.Repositories;
+
+public class IdentityContext : DbContext
+{
+    public DbSet<UserModel> Users { get; set; } = null!;
+    public DbSet<RoleModel> Roles { get; set; } = null!;
+    public DbSet<RoleClaimModel> RoleClaims { get; set; } = null!;
+    public DbSet<UserClaimModel> UserClaims { get; set; } = null!;
+    public DbSet<RoleUserClaimModel> RoleUsersClaims { get; set; } = null!;
+    public DbSet<CompanyModel> Companies { get; set; } = null!;
+    public DbSet<CompanyUserClaimModel> CompanyUsersClaims { get; set; } = null!;
+}
