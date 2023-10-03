33667a6bcd595852371a5552d3aebf5ab428f9aa
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-02 23:17:01 -0300

    Create Create user handler #31 - request and response

--- src/Services/Identity/Ecommerce.Identity.Application/Commands/User/CreateUser/CreateUserRequest.cs
+++ src/Services/Identity/Ecommerce.Identity.Application/Commands/User/CreateUser/CreateUserRequest.cs
@@ -2,4 +2,13 @@
 
 public class CreateUserRequest
 {
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string? NickName { get; set; }
+
+    /// <summary>
+    /// Phone number
+    /// </summary>
+    public string? PhoneNumber { get; set; }
+    public string Password { get; set; } = string.Empty;
 }
--- src/Services/Identity/Ecommerce.Identity.Application/Commands/User/CreateUser/CreateUserResponse.cs
+++ src/Services/Identity/Ecommerce.Identity.Application/Commands/User/CreateUser/CreateUserResponse.cs
@@ -2,4 +2,39 @@
 
 public class CreateUserResponse
 {
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string? NickName { get; set; }
+    public bool EmailConfirmed { get; set; }
+
+    /// <summary>
+    /// Phone number
+    /// </summary>
+    public string? PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Is phone number confirmed
+    /// </summary>
+    public bool PhoneNumberConfirmed { get; set; }
+
+    /// <summary>
+    /// Is two factory enabled
+    /// </summary>
+    public bool TwoFactoryEnabled { get; set; }
+
+    /// <summary>
+    /// Date until lockout
+    /// </summary>
+    public DateTime? LockOutEnd { get; set; }
+
+    /// <summary>
+    /// Is lockout enabled
+    /// </summary>
+    public bool LockOutEnabled { get; set; }
+
+    /// <summary>
+    /// Count of fails access
+    /// </summary>
+    public int AccessFailedCount { get; set; }
 }
