0e319bbfb89ca40d32a611651cf681b2f2bb48f9
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-04 16:36:22 -0300

    Create company command handler #41 - repair class

--- src/Services/Identity/Ecommerce.Identity.Application/Model/CompanymODEL.cs
+++ src/Services/Identity/Ecommerce.Identity.Application/Model/CompanymODEL.cs
@@ -4,6 +4,6 @@ public class CompanyModel
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public DateTime? UpdateAt { getprivate set; }
+    public DateTime? UpdateAt { get; set; }
     public DateTime CreateAt { get; set; }
 }
