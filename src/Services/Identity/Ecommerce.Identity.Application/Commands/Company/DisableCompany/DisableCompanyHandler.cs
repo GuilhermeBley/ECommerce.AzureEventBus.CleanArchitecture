7cb9b343a00cb84cc4f0b326d3707a57dd8f1111
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-08 21:38:18 -0300

    Disable company handler #44 - handler

--- src/Services/Identity/Ecommerce.Identity.Application/Commands/Company/DisableCompany/DisableCompanyHandler.cs
+++ src/Services/Identity/Ecommerce.Identity.Application/Commands/Company/DisableCompany/DisableCompanyHandler.cs
@@ -1,12 +1,38 @@
 ﻿using Ecommerce.Identity.Application.Mediator;
+using Ecommerce.Identity.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Identity.Application.Commands.Company.DisableCompany;
 
 public class DisableCompanyHandler
     : IAppRequestHandler<DisableCompanyRequest, Result<DisableCompanyResponse>>
 {
-    public Task<Result<DisableCompanyResponse>> Handle(DisableCompanyRequest request, CancellationToken cancellationToken)
+    private readonly IdentityContext _identityContext;
+
+    public DisableCompanyHandler(IdentityContext identityContext)
     {
-        throw new NotImplementedException();
+        _identityContext = identityContext;
+    }
+
+    public async Task<Result<DisableCompanyResponse>> Handle(DisableCompanyRequest request, CancellationToken cancellationToken)
+    {
+        var companyFound =
+            await _identityContext.Companies.FirstOrDefaultAsync(c => c.Id == request.CompanyId);
+
+        if (companyFound is null)
+            return ResultBuilderExtension.CreateFailed<DisableCompanyResponse>(ErrorEnum.CompanyNotFound);
+
+        companyFound.Disabled = true;
+        companyFound.UpdateAt = DateTime.UtcNow;
+
+        await _identityContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(new DisableCompanyResponse
+        {
+            CreateAt = companyFound.CreateAt,
+            UpdateAt = companyFound.UpdateAt.Value,
+            Id = companyFound.Id,
+            Name = companyFound.Name,
+        });
     }
 }
--- src/Services/Identity/Ecommerce.Identity.Application/Model/CompanymODEL.cs
+++ src/Services/Identity/Ecommerce.Identity.Application/Model/CompanymODEL.cs
@@ -4,6 +4,7 @@ public class CompanyModel
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public bool Disabled { get; set; }
     public DateTime? UpdateAt { get; set; }
     public DateTime CreateAt { get; set; }
 }
