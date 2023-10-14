30c7104c2e5b418a73767d0c3b64aa9af683c447
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-14 00:10:04 -0300

    Create notifications #33 - CompanyUpdatedEvent

---
+++ src/BuildingBlocks/EventBus/ECommerce.EventBus/Events/CompanyUpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.EventBus.Events;
+
+public class CompanyUpdatedEvent : IntegrationEvent
+{
+
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime UpdateAt { get; set; }
+    public DateTime CreateAt { get; set; }
+}
