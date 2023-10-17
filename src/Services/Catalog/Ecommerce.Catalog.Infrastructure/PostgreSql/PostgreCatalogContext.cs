8e58001b88a7dfd00e1072233cbbcf94acf1ab42
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-16 23:30:15 -0300

    Create infrastructure identity project #49 - PostgreCatalogContext

--- src/Services/Catalog/Ecommerce.Catalog.Infrastructure/PostgreSql/PostgreCatalogContext.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Infrastructure/PostgreSql/PostgreCatalogContext.cs
@@ -38,11 +38,19 @@ public class PostgreCatalogContext : CatalogContext
         {
             builder.HasKey(x => x.Id);
             builder.HasIndex(x => x.Email).IsUnique();
+
+            builder.Property(x => x.Email).HasColumnType("varchar(500)");
+            builder.Property(x => x.Name).HasColumnType("varchar(500)");
+            builder.Property(x => x.NickName).HasColumnType("varchar(500)");
+            builder.Property(x => x.PasswordHash).HasColumnType("varchar(500)");
+            builder.Property(x => x.PasswordSalt).HasColumnType("varchar(500)");
         });
 
         modelBuilder.Entity<Application.Model.Company.CompanyModel>(builder =>
         {
             builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasColumnType("varchar(500)");
         });
 
         modelBuilder.Entity<Application.Model.Company.CompanyProductModel>(builder =>
@@ -65,11 +73,17 @@ public class PostgreCatalogContext : CatalogContext
                 .HasOne<UserModel>()
                 .WithMany()
                 .HasForeignKey(x => x.UserId);
+
+            builder.Property(x => x.ClaimType).HasColumnType("varchar(255)");
+            builder.Property(x => x.ClaimValue).HasColumnType("varchar(255)");
         });
 
         modelBuilder.Entity<Application.Model.Product.ProductModel>(builder =>
         {
             builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasColumnType("varchar(255)");
+            builder.Property(x => x.Description).HasColumnType("varchar(1000)");
         });
     }
 }
