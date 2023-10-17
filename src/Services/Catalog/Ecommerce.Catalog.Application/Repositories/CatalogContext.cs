83c594c9dada2be53cbbb1a29d490911174ff993
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-17 16:26:46 -0300

    Create infrastructure identity project #49 - remove unused tables

--- src/Services/Catalog/Ecommerce.Catalog.Application/Repositories/CatalogContext.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Repositories/CatalogContext.cs
@@ -9,12 +9,7 @@ namespace Ecommerce.Catalog.Application.Repositories;
 public class CatalogContext : DbContext
 {
     public DbSet<UserModel> Users { get; set; } = null!;
-    public DbSet<RoleModel> Roles { get; set; } = null!;
-    public DbSet<RoleClaimModel> RoleClaims { get; set; } = null!;
-    public DbSet<UserClaimModel> UserClaims { get; set; } = null!;
-    public DbSet<RoleUserClaimModel> RoleUsersClaims { get; set; } = null!;
     public DbSet<ProductModel> Products { get; set; } = null!;
     public DbSet<CompanyModel> Companies { get; set; } = null!;
-    public DbSet<CompanyUserClaimModel> CompanyUsersClaims { get; set; } = null!;
     public DbSet<CompanyProductModel> CompanyProducts { get; set; } = null!;
 }
