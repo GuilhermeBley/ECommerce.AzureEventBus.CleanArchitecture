7f44aa766e66824d2b6ce23732e17abffa3bc352
Guilherme Bley <guilhermecbley@gmail.com>
2023-10-30 23:51:21 -0300

    Mysql identity, change string size #57 - add varchar type

--- src/Services/Identity/Ecommerce.Identity.Infrastructure/Context/MySqlDbContext.cs
+++ src/Services/Identity/Ecommerce.Identity.Infrastructure/Context/MySqlDbContext.cs
@@ -41,12 +41,16 @@ public class MySqlDbContext : IdentityContext
                 .HasOne<RoleModel>()
                 .WithMany()
                 .HasForeignKey(x => x.IdRole);
+            builder.Property(p => p.ClaimType).HasColumnType("VARCHAR").HasMaxLength(255);
+            builder.Property(p => p.ClaimValue).HasColumnType("VARCHAR").HasMaxLength(255);
         });
 
         modelBuilder.Entity<Application.Model.RoleModel>(builder =>
         {
             builder.HasKey(x => x.Id);
             builder.HasIndex(x => x.NormalizedName).IsUnique();
+            builder.Property(p => p.Name).HasColumnType("VARCHAR").HasMaxLength(255);
+            builder.Property(p => p.NormalizedName).HasColumnType("VARCHAR").HasMaxLength(255);
         });
 
         modelBuilder.Entity<Application.Model.RoleUserClaimModel>(builder =>
@@ -65,17 +69,26 @@ public class MySqlDbContext : IdentityContext
                 .HasOne<UserModel>()
                 .WithMany()
                 .HasForeignKey(x => x.UserId);
+            builder.Property(p => p.ClaimType).HasColumnType("VARCHAR").HasMaxLength(255);
+            builder.Property(p => p.ClaimValue).HasColumnType("VARCHAR").HasMaxLength(255);
         });
 
         modelBuilder.Entity<Application.Model.UserModel>(builder =>
         {
             builder.HasKey(x => x.Id);
             builder.HasIndex(x => x.Email).IsUnique();
+            builder.Property(p => p.Name).HasColumnType("VARCHAR").HasMaxLength(500);
+            builder.Property(p => p.NickName).HasColumnType("VARCHAR").HasMaxLength(500);
+            builder.Property(p => p.Email).HasColumnType("VARCHAR").HasMaxLength(500);
+            builder.Property(p => p.PhoneNumber).HasColumnType("VARCHAR").HasMaxLength(50);
+            builder.Property(p => p.PasswordHash).HasColumnType("VARCHAR").HasMaxLength(255);
+            builder.Property(p => p.PasswordSalt).HasColumnType("VARCHAR").HasMaxLength(255);
         });
 
         modelBuilder.Entity<Application.Model.CompanyModel>(builder =>
         {
             builder.HasKey(x => x.Id);
+            builder.Property(p => p.Name).HasColumnType("VARCHAR").HasMaxLength(500);
         });
 
         modelBuilder.Entity<Application.Model.CompanyUserClaimModel>(builder =>
