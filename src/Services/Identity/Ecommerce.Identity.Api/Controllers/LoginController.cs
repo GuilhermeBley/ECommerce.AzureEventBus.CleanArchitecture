5e9bff3ef90896d158dcd992284872c1bdb9b418
Guilherme Bley <guilhermecbley@gmail.com>
2023-11-03 21:48:45 -0300

    Login controller #19 - resend email confirmation

--- src/Services/Identity/Ecommerce.Identity.Api/Controllers/LoginController.cs
+++ src/Services/Identity/Ecommerce.Identity.Api/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 using Ecommerce.Identity.Application.Commands.Authentication.LoginUser;
+using Ecommerce.Identity.Application.Commands.Authentication.ResendEmailConfirmation;
 using Ecommerce.Identity.Application.Mediator;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,9 +23,22 @@ public class LoginController : ControllerBase
     {
         var loginResult = await _appMediator.Send<LoginUserRequest, Result<LoginUserResponse>>(model, cancellationToken);
 
-        if (loginResult.IsSuccess)
-            return Ok(loginResult.Value);
+        if (loginResult.TryGetValue(out var result))
+            return Ok(result);
 
         return Unauthorized();
+    }
+
+    [HttpPost("resendEmailConfirmation")]
+    public async Task<IActionResult> LoginAsync(
+        ResendEmailConfirmationRequest model,
+        CancellationToken cancellationToken = default)
+    {
+        var emailResendResult = await _appMediator.Send<ResendEmailConfirmationRequest, Result<ResendEmailConfirmationResponse>>(model, cancellationToken);
+
+        if (emailResendResult.TryGetValue(out var result))
+            return Ok(result);
+
+        return BadRequest(emailResendResult.Errors);
     }
 }
