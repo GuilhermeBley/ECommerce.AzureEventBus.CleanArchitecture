932ea3dd180f1b0dead39cbebeff62046f95cfd1
Guilherme Bley <guilhermecbley@gmail.com>
2023-11-07 00:00:52 -0300

    User controller #20 - create user

--- src/Services/Identity/Ecommerce.Identity.Api/Controllers/UserController.cs
+++ src/Services/Identity/Ecommerce.Identity.Api/Controllers/UserController.cs
@@ -1,10 +1,13 @@
+using Ecommerce.Identity.Application.Commands.User.CreateUser;
 using Ecommerce.Identity.Application.Mediator;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Identity.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class UserController : ControllerBase
 {
     private readonly IAppMediator _appMediator;
@@ -12,5 +15,18 @@ public class UserController : ControllerBase
     public UserController(IAppMediator appMediator)
     {
         _appMediator = appMediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CreateUserResponse>> CreateAsync(
+        CreateUserRequest model, 
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _appMediator.Send<CreateUserRequest, Result<CreateUserResponse>>(model, cancellationToken);
+
+        if (result.IsSuccess)
+            return Created($"api/User/{result.Value?.Id}", result.Value);
+
+        return BadRequest(result.Errors);
     }
 }
