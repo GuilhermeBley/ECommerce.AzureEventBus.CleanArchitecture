b259068551abb4f805bebf9a4da13d38f070f55a
Guilherme Bley <guilhermecbley@gmail.com>
2023-11-08 22:23:41 -0300

    remove unecessary usings

--- src/Services/Identity/Ecommerce.Identity.Core/Primitive/Result/Result.cs
+++ src/Services/Identity/Ecommerce.Identity.Core/Primitive/Result/Result.cs
@@ -1,6 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using static Ecommerce.Identity.Core.Primitive.Result.ResultBuilder;
 
 namespace Ecommerce.Identity.Core.Primitive.Result;
 
