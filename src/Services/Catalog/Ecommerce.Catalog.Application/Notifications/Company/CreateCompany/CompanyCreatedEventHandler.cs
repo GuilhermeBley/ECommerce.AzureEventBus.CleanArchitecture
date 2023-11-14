5d02244261f24d5347fe755ca80f4a0acbc782cb
Guilherme Bley <guilhermecbley@gmail.com>
2023-11-13 23:13:38 -0300

    Create event about CreateUserEvent in catalog API #60

---
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Notifications/Company/CreateCompany/CompanyCreatedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Catalog.Application.Repositories;
+using Ecommerce.EventBus.Events;
+
+namespace Ecommerce.Catalog.Application.Notifications.Company.CreateCompany;
+
+public class CompanyCreatedEventHandler : IIntegrationEventHandler<CompanyCreatedEvent>
+{
+    private readonly CatalogContext _catalogContext;
+
+    public CompanyCreatedEventHandler(CatalogContext catalogContext)
+    {
+        _catalogContext = catalogContext;
+    }
+
+    public async Task Handle(CompanyCreatedEvent @event)
+    {
+        await using var transaction = await _catalogContext.Database.BeginTransactionAsync();
+
+        if (_catalogContext.Companies.FirstOrDefault(c => c.Id == @event.Id) is not null)
+            return;
+
+        await _catalogContext.Companies.AddAsync(new Model.Company.CompanyModel
+        {
+            CreateAt = DateTime.UtcNow,
+            Id = @event.Id,
+            Name = @event.Name,
+            UpdateAt = DateTime.UtcNow,
+        });
+
+        await transaction.CommitAsync();
+
+        await _catalogContext.SaveChangesAsync();
+    }
+}
