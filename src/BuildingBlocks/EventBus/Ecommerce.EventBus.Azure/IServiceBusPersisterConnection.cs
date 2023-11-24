5afb7f5f81c90cd1a4c25ef0a714640b0a3235e8
Guilherme Bley <guilhermecbley@gmail.com>
2023-11-23 23:59:02 -0300

    Bug failed to subscribe in topic - add rule manager and processor to persistent connection #66

--- src/BuildingBlocks/EventBus/Ecommerce.EventBus.Azure/IServiceBusPersisterConnection.cs
+++ src/BuildingBlocks/EventBus/Ecommerce.EventBus.Azure/IServiceBusPersisterConnection.cs
@@ -5,5 +5,7 @@ namespace Ecommerce.EventBus.Azure
     public interface IServiceBusPersisterConnection : IDisposable, IAsyncDisposable
     {
         ServiceBusSender CreateModel();
+        ServiceBusProcessor SubscriptionProcessor { get; }
+        ServiceBusRuleManager SubscriptionRuleManager { get; }
     }
 }
