2494dd969dc7bfa3709911c25f14823a44ec8473
Guilherme Bley <guilhermecbley@gmail.com>
2023-12-01 23:26:00 -0300

    add log to CompanyCreatedEventHandler

--- src/Services/Catalog/Ecommerce.Catalog.Application/Notifications/Company/CreateCompany/CompanyCreatedEventHandler.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Notifications/Company/CreateCompany/CompanyCreatedEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.Catalog.Application.Repositories;
 using Ecommerce.EventBus.Events;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.Catalog.Application.Notifications.Company.CreateCompany;
 
@@ -9,29 +10,44 @@ namespace Ecommerce.Catalog.Application.Notifications.Company.CreateCompany;
 public class CompanyCreatedEventHandler : IIntegrationEventHandler<CompanyCreatedEvent>
 {
     private readonly CatalogContext _catalogContext;
+    private readonly ILogger _logger;
 
-    public CompanyCreatedEventHandler(CatalogContext catalogContext)
+    public CompanyCreatedEventHandler(
+        CatalogContext catalogContext,
+        ILogger<CompanyCreatedEventHandler> logger)
     {
         _catalogContext = catalogContext;
+        _logger = logger;
     }
 
     public async Task Handle(CompanyCreatedEvent @event)
     {
-        await using var transaction = await _catalogContext.Database.BeginTransactionAsync();
-
-        if (_catalogContext.Companies.FirstOrDefault(c => c.Id == @event.Id) is not null)
-            return;
-
-        await _catalogContext.Companies.AddAsync(new Model.Company.CompanyModel
+        try
         {
-            CreateAt = DateTime.UtcNow,
-            Id = @event.Id,
-            Name = @event.Name,
-            UpdateAt = DateTime.UtcNow,
-        });
+            await using var transaction = await _catalogContext.Database.BeginTransactionAsync();
 
-        await transaction.CommitAsync();
+            if (_catalogContext.Companies.FirstOrDefault(c => c.Id == @event.Id) is not null)
+                return;
 
-        await _catalogContext.SaveChangesAsync();
+            await _catalogContext.Companies.AddAsync(new Model.Company.CompanyModel
+            {
+                CreateAt = DateTime.UtcNow,
+                Id = @event.Id,
+                Name = @event.Name,
+                UpdateAt = DateTime.UtcNow,
+            });
+
+            await transaction.CommitAsync();
+
+            await _catalogContext.SaveChangesAsync();
+
+            _logger.LogTrace("Company {0} created.", @event.Id);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to create company {0}.", @event.Id);
+
+            throw;
+        }
     }
 }
