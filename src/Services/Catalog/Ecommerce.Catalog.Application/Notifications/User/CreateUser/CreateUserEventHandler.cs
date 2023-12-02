a028fa5d7d271bdcf92741184367731a9eb4d5e7
Guilherme Bley <guilhermecbley@gmail.com>
2023-12-01 23:29:51 -0300

    add log to CreateUserEventHandler

--- src/Services/Catalog/Ecommerce.Catalog.Application/Notifications/User/CreateUser/CreateUserEventHandler.cs
+++ src/Services/Catalog/Ecommerce.Catalog.Application/Notifications/User/CreateUser/CreateUserEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.Catalog.Application.Repositories;
 using Ecommerce.EventBus.Events;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.Catalog.Application.Notifications.User.CreateUser;
 
@@ -7,29 +8,44 @@ public class CreateUserEventHandler
     : IIntegrationEventHandler<CreateUserEvent>
 {
     private readonly CatalogContext _catalogContext;
+    private readonly ILogger _logger;
 
-    public CreateUserEventHandler(CatalogContext catalogContext)
+    public CreateUserEventHandler(
+        CatalogContext catalogContext, 
+        ILogger<CreateUserEventHandler> logger)
     {
         _catalogContext = catalogContext;
+        _logger = logger;
     }
 
     public async Task Handle(CreateUserEvent @event)
     {
-        await using var transaction = await _catalogContext.Database.BeginTransactionAsync();
-
-        await _catalogContext.Users.AddAsync(new Model.Identity.UserModel
+        try
         {
-            Email = @event.Email,
-            EmailConfirmed = false,
-            Id = @event.Id,
-            Name = @event.Name,
-            NickName = @event.NickName,
-            PasswordHash = string.Empty,
-            PasswordSalt = string.Empty,
-        });
+            await using var transaction = await _catalogContext.Database.BeginTransactionAsync();
 
-        await transaction.CommitAsync();
+            await _catalogContext.Users.AddAsync(new Model.Identity.UserModel
+            {
+                Email = @event.Email,
+                EmailConfirmed = false,
+                Id = @event.Id,
+                Name = @event.Name,
+                NickName = @event.NickName,
+                PasswordHash = string.Empty,
+                PasswordSalt = string.Empty,
+            });
 
-        await _catalogContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+
+            await _catalogContext.SaveChangesAsync();
+
+            _logger.LogTrace("User {0} created.", @event.Id);
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogError(ex, "Failed to create company {0}.", @event.Id);
+
+            throw;
+        }
     }
 }
